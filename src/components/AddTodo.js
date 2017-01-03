8dc2b25864cd4d2920274ecc8e36b38099f704c4
Eduardo Reche <edu.reche@gmail.com>
2017-01-03 11:40:32 -0200

    Added bootstrap

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -6,19 +6,24 @@ let AddTodo = ({ dispatch }) => {
   let input;
 
   return (
-    <form>
-      <input ref={node => {
-         input = node;
-        }} />
-        {' '}
-        <button type="submit" onClick={(e) => {
+    <form className="form-inline">
+      <div className="form-group">
+        <input 
+          className="form-control"
+          ref={node => {
+            input = node;
+          }} />
+        <button type="submit" 
+          className="btn btn-default"
+          onClick={(e) => {
             e.preventDefault();
             dispatch(addTodo(input.value));
             input.value = '';
           }
         }>
-          Add Todo
+          Add
         </button>
+      </div>
     </form>
   )
 };
